5cb8c83d1b4abb1dff79431d0553ece6160ce9ed
Gustavo de Oliveira Barbosa <167383332+Guhbrsa@users.noreply.github.com>
2024-09-08 00:41:03 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -19,5 +19,26 @@ let dados = [
         descricao: "Rebeca Andrade é uma ginasta brasileira que conquistou o mundo com suas acrobacias e força. Aos poucos anos, ela já coleciona títulos e é considerada uma das maiores ginastas da história do Brasil.",
         link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
         tags: "ginástica ginastica ouro"
+    },
+    {
+        titulo: "Neymar Jr",
+        rede: "https://www.instagram.com/neymarjr/",
+        descricao: "Neymar Jr. é um dos maiores jogadores de futebol da atualidade, conhecido por sua habilidade e carisma.",
+        link: "https://pt.wikipedia.org/wiki/Neymar",
+        tags: "fute futebol fut football ball foot"
+    },
+    {
+        titulo: "Darlan Souza",
+        rede: "https://www.instagram.com/darlan.souza01/",
+        descricao: "Darlan Ferreira Souza é um jogador de voleibol brasileiro que atua na posição de oposto.",
+        link: "https://pt.wikipedia.org/wiki/Darlan_Souza",
+        tags: "voleibol volei vôlei volebas"
+    },
+    {
+        titulo: "FalleN (Gabriel Toledo)",
+        rede: "https://www.instagram.com/fallen/",
+        descricao: "profissional de Counter-Strike brasileiro. Atualmente joga pela FURIA. Ele foi nomeado como a pessoa mais influente do esporte eletrônico brasileiro.",
+        link: "https://pt.wikipedia.org/wiki/FalleN",
+        tags: "e-sport cs cs-go cs go fps "
     }
 ];
